6c3da17fecd52e1baec9dbcd4e632eb36f3363cb
Kavin Teenakul <andythe_great@protonmail.com>
2022-12-06 20:31:16 +0100

    fix import

--- H2_stor_ver2.py
+++ H2_stor_ver2.py
@@ -10,7 +10,6 @@ tot_veh_E = 26.5732# * 0.5
 
 stor_night_charge_time = 12 #hr
 colors = ('red','blue','green','darkorange')
-# print(colors[2])
 ec = 0.8
 fc = 0.6
 
@@ -28,7 +27,6 @@ storage_charge_time = 3 #hr
 
 mw_lst = [1,2,3]
 runtime = np.arange(0,1,0.001)
-storage_lst = []
 
 for i in mw_lst:
     ###########
@@ -44,9 +42,9 @@ for i in mw_lst:
     extra_h2_night = np.where(need_h2_1c-max_h2_stor > 0,need_h2_1c-max_h2_stor,0)
     extra_h2_need_3c = extra_h2_night*3
     h2_prod_night = stor_night_charge_time * grid * grid2h
-    import_h2 = np.where(extra_h2_need_3c - h2_prod_night>0,extra_h2_need_3c - h2_prod_night,0)
-    tot_h2_stor_size = np.where(h2_prod_night+max_h2_stor>need_h2_1c*3,h2_prod_night+max_h2_stor,need_h2_1c*3)
-    # plt.plot(runtime,import_h2,"--",color=colors[np.argmin(np.abs(np.array(mw_lst)-i))],linewidth=linewidth)
+    import_h2 = need_h2_1c*3-(h2_prod_night+max_h2_stor)
+    tot_h2_stor_size = np.where(h2_prod_night+max_h2_stor<need_h2_1c*3,h2_prod_night+max_h2_stor,need_h2_1c*3)
+    plt.plot(runtime,import_h2,"--",color=colors[np.argmin(np.abs(np.array(mw_lst)-i))],linewidth=linewidth)
     plt.plot(runtime,tot_h2_stor_size,"-",color=colors[np.argmin(np.abs(np.array(mw_lst)-i))],linewidth=linewidth)
     
 plt.xticks(np.arange(0, 1+0.1, step=0.1))
@@ -57,8 +55,8 @@ plt.xlim(0,1)
 storage_cap = Line2D([0], [0], label='Storage Capacities', color='black', linestyle='-',linewidth=linewidth)
 import_h2 = Line2D([0], [0], label='Imported Hydrogen', color='black', linestyle='--',linewidth=linewidth)
 grid1 = mpatches.Patch(label='1 MW Grid', color='red')
-grid2 = mpatches.Patch(label='1 MW Grid', color='blue')
-grid3 = mpatches.Patch(label='2 MW Grid', color='green')
+grid2 = mpatches.Patch(label='2 MW Grid', color='blue')
+grid3 = mpatches.Patch(label='3 MW Grid', color='green')
 plt.legend(handles=[storage_cap,import_h2,grid1,grid2,grid3],ncol=3,loc='upper center')
 plt.xlabel('Runtime fraction')
 plt.ylabel('Energy Storage Capacities/Imported Hydrogen (MWh)')
